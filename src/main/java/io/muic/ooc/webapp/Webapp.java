36a357d8d6161744b62f11af1b1902a3b49f4044
suprakit <winchon29@gmail.com>
2021-07-15 23:32:16 +0700

    Sucessfully added jstl taglib by upgradingtomcat to new version. This allows us to use taglib in jsp

--- src/main/java/io/muic/ooc/webapp/Webapp.java
+++ src/main/java/io/muic/ooc/webapp/Webapp.java
@@ -34,7 +34,7 @@ public class Webapp {
 
             tomcat.start();
             tomcat.getServer().await();
-        } catch (ServletException | LifecycleException ex) {
+        } catch (LifecycleException ex) {
             ex.printStackTrace();
         }
 
